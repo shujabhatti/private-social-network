0f71e0da3e1c7226707df834f724450bf4387906
Muhammad Shuja <shuja2039@gmail.com>
2020-08-09 19:03:28 +0500

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.json({ msg: "Hello Node" }));
+
+// Define Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running at ${PORT}...`));
