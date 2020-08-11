219ab8281a44764bb33f88da1e6b8b619c53f732
Muhammad Shuja <shuja2039@gmail.com>
2020-08-11 23:55:18 +0500

    User authentication completed

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.json({ msg: "Hello Node" }));
 
