1b5d0d11ef5e0571078d314759922ab1596f8f3b
shujabhatti <58146082+shujabhatti@users.noreply.github.com>
2020-10-11 23:22:16 +0500

    prepare to deploy

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const connectDB = require("./config/db");
-var cors = require('cors');
-
-app.use(cors());
 
 connectDB();
 
